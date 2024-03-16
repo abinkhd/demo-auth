0b8843f608c4d00370ae5c4729681e32edf41111
abinkhd <abinkhd@gmail.com>
2024-03-16 18:36:50 +0530

    port chnages

--- index.js
+++ index.js
@@ -11,4 +11,7 @@ app.use(cors());
 
 app.use("/api/", authRoute);
 
-app.listen(400, () => console.log("Server up and running"));
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
