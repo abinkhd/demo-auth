3d47f29a3529904155cdcf172f4abcafa65f96bd
abinkhd <abinkhd@gmail.com>
2024-03-16 18:17:49 +0530

    added cors

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
 
 //import routes
 const authRoute = require("./routes/auth");
 app.use(express.json());
+app.use(cors());
+
 //Router Middleware
+
 app.use("/api/", authRoute);
 
 app.listen(4000, () => console.log("Server up and running"));
